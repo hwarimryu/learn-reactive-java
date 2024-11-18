52d90a640c039a0523bd04dc90b146b53bb5adbc
hwarimryu <fwarim@gmail.com>
2024-11-18 12:53:18 +0900

    init

---
+++ src/main/java/com/study/hwarim/reactive/StudyReactiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.study.hwarim.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyReactiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyReactiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/hwarim/reactive/StudyReactiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.hwarim.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyReactiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
